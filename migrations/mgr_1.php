e21ccf00317b09a1531e5a8908382b8621544014
martti <info@martti.be>
2019-01-18 11:47:59 +0100

    (c) 2019

--- migrations/mgr_1.php
+++ migrations/mgr_1.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * phpBB Extension - marttiphpbb Calendar Auto Archive
-* @copyright (c) 2018 marttiphpbb <info@martti.be>
+* @copyright (c) 2018 - 2019 marttiphpbb <info@martti.be>
 * @license GNU General Public License, version 2 (GPL-2.0)
 */
 
